45f24f6d0fad5f028aeb29ab0bf7eed453e7a787
Nji Franclin Muluh <mulfranck5@gmail.com>
2022-08-22 19:57:04 +0100

    added menu sections (#4)
    * add menu section
    
    * chore: removed animate.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,31 @@
 'use strict'
 
- 
+const $openNav = document.querySelector('[data-bs-toggle="offcanvas"]');
+const $closeNav = document.querySelector('[data-bs-dismiss="offcanvas"]');
+const $navItems = document.querySelectorAll('.nav-item');
+const $socialIcons = document.querySelector('.social-media');
+
+
+$openNav.addEventListener('click', e => {
+
+    $navItems.forEach(($navItem, ind) => {
+        setTimeout(() => {
+            $navItem.classList.add('xyz-in')
+            $socialIcons.classList.add('xyz-in')
+            $navItem.classList.remove('invisible')
+            $socialIcons.classList.remove('invisible');
+        }, 300)
+    });
+    
+});
+
+$closeNav.addEventListener('click', e => {
+    console.log(e.target)
+    $navItems.forEach(($navItem, ind) => {
+        $socialIcons.classList.remove('xyz-in');
+        $navItem.classList.remove('xyz-in')
+        $socialIcons.classList.add('invisible');
+        $navItem.classList.add('invisible')
+    });
+})
 
