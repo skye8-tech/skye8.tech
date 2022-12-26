0c6997f203839e796fecca1106065a4fb9938940
mulfranck <njifranclin@gmail.com>
2022-12-26 13:25:33 +0100

    add background on scroll

--- src/js/services.js
+++ src/js/services.js
@@ -1,5 +1,6 @@
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
+let $navbar = document.querySelector("#navbar");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -12,8 +13,12 @@ function scrollFunction() {
     document.documentElement.scrollTop > 20
   ) {
     mybutton.style.display = "block";
+    $navbar.classList.add('bg-dark');
+    $navbar.classList.add('navbar-dark');
   } else {
     mybutton.style.display = "none";
+    $navbar.classList.remove('bg-dark');
+    $navbar.classList.remove('navbar-dark');
   }
 }
 // When the user clicks on the button, scroll to the top of the document
